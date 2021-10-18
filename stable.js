2634bcb21dfdca11ae43729d3c7e980de24b953b
Daniel Rajtšlégr <80426398+Molenusaczech@users.noreply.github.com>
2021-10-18 09:27:44 +0200

    Update stable.js

--- stable.js
+++ stable.js
@@ -1,79 +1,82 @@
-def on_button_pressed_a():
-    global snakeDirection
-    if snakeDirection == 0:
+input.onButtonPressed(Button.A, function () {
+    if (snakeDirection == 0) {
         snakeDirection = 3
-    else:
+    } else {
         snakeDirection += -1
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    global snakeDirection
-    if snakeDirection == 3:
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (snakeDirection == 3) {
         snakeDirection = 0
-    else:
+    } else {
         snakeDirection += 1
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-tempY = 0
-tempX = 0
-lastY = 0
-lastX = 0
-snakeDirection = 0
+    }
+})
+let tempY = 0
+let tempX = 0
+let lastY = 0
+let lastX = 0
+let snakeDirection = 0
 snakeDirection = 1
-headX = 2
-headY = 2
-SnakeCellsX = [1]
-SnakeCellsY = [2]
-fruitX = 3
-fruitY = 3
-led.plot_brightness(3, 3, 130)
-fruitSet = 1
-led.set_display_mode(DisplayMode.GREYSCALE)
-
-def on_forever():
-    global fruitX, fruitY, headY, headX, lastX, lastY, fruitSet, tempX, tempY
-    index = 0
-    while index <= len(SnakeCellsX):
-        if SnakeCellsX[index] == headX:
-            if SnakeCellsY[index] == headY:
+let headX = 2
+let headY = 2
+let SnakeCellsX = [1]
+let SnakeCellsY = [2]
+let fruitX = 3
+let fruitY = 3
+led.plotBrightness(3, 3, 130)
+let fruitSet = 1
+led.setDisplayMode(DisplayMode.Greyscale)
+basic.forever(function () {
+    for (let index = 0; index <= SnakeCellsX.length; index++) {
+        if (SnakeCellsX[index] == headX) {
+            if (SnakeCellsY[index] == headY) {
                 fruitX = -1
                 fruitY = -1
-                while True:
-                    basic.show_string("Game Over!")
-        index += 1
-    if headX >= 5 or headY >= 5 or (headY <= -1 or headX <= -1):
-        while True:
+                while (true) {
+                    basic.showString("Game Over!")
+                }
+            }
+        }
+    }
+    if (headX >= 5 || headY >= 5 || (headY <= -1 || headX <= -1)) {
+        while (true) {
             fruitX = -1
             fruitY = -1
-            basic.show_string("Game Over!")
+            basic.showString("Game Over!")
+        }
+    }
     led.plot(headX, headY)
     SnakeCellsX.unshift(headX)
     SnakeCellsY.unshift(headY)
-    control.wait_micros(1000000)
-    if snakeDirection == 0:
+    control.waitMicros(1000000)
+    if (snakeDirection == 0) {
         headY += -1
-    elif snakeDirection == 1:
+    } else if (snakeDirection == 1) {
         headX += 1
-    elif snakeDirection == 2:
+    } else if (snakeDirection == 2) {
         headY += 1
-    elif snakeDirection == 3:
+    } else if (snakeDirection == 3) {
         headX += -1
-    if not (headX == fruitX and headY == fruitY):
+    }
+    if (!(headX == fruitX && headY == fruitY)) {
         lastX = SnakeCellsX.pop()
         lastY = SnakeCellsY.pop()
         led.unplot(lastX, lastY)
-    else:
+    } else {
         fruitSet = 0
-        while fruitSet == 0:
+        while (fruitSet == 0) {
             tempX = randint(0, 4)
             tempY = randint(0, 4)
-            if not (convert_to_text(SnakeCellsX).includes(convert_to_text(tempX)) and (convert_to_text(SnakeCellsY).includes(convert_to_text(tempY)) and (tempX == headX and tempY == headY))):
+            if (!(convertToText(SnakeCellsX).includes(convertToText(tempX)) && (convertToText(SnakeCellsY).includes(convertToText(tempY)) && (tempX == headX && tempY == headY)))) {
                 fruitX = tempX
                 fruitY = tempY
-                led.plot_brightness(fruitX, fruitY, 130)
+                led.plotBrightness(fruitX, fruitY, 130)
                 fruitSet = 1
-basic.forever(on_forever)
-
-def on_forever2():
-    pass
-basic.forever(on_forever2)
+            }
+        }
+    }
+})
+basic.forever(function () {
+	
+})
